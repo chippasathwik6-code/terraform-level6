97cc6a949084d1141044f2370d0a6939dd9a8fed
sathwik chippa <sathwikchippa@sathwiks-MacBook-Air.local>
2026-05-15 00:58:50 +0100

    level 6 working setup

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "vm" {
   ami           = "ami-0c1c30571d2dae5c9"
-instance_type = "t3.micro"
+instance_type = "t2.nano"
   tags = {
     Name = "Level6-VM"
   }
