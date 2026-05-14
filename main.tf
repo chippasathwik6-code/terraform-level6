66ea6b40f6217dac5160534bd109a9d0e066f7a9
sathwik chippa <sathwikchippa@sathwiks-MacBook-Air.local>
2026-05-15 00:43:27 +0100

    level 6 clean setup

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0c1c30571d2dae5c9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Level6-VM"
+  }
+}
