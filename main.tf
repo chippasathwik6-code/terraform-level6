253802ee79ae6b12afe0a410122aff19674847a6
chippasathwik6-code <chippasathwik6@gmail.com>
2026-05-15 01:22:57 +0100

    Update AWS instance AMI and instance name

--- main.tf
+++ main.tf
@@ -2,11 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "vm" {
-  ami           = "ami-0c7217cdde317cfec"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "done sleep"
+    Name = "done-sleep"
   }
 }
