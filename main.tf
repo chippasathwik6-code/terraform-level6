c033caba3f6e11987bf4416d70cadf4432b8ea04
sathwik chippa <sathwikchippa@sathwiks-MacBook-Air.local>
2026-05-15 01:03:55 +0100

    fix region to avoid vcpu limit

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "vm" {
   ami           = "ami-0c1c30571d2dae5c9"
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Level6-VM"
