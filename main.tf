4c5e016b22bd1964b3c2ec14ee5760facc94671e
sathwik chippa <sathwikchippa@sathwiks-MacBook-Air.local>
2026-05-15 01:46:52 +0100

    fix AMI issue using dynamic lookup

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 }
 
 resource "aws_instance" "vm" {
-  ami           = "ami-0c7217cdde317cfec"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
