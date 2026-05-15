97e2316cfb0eef27f0fe231668f503948bcfe685
chippasathwik6-code <chippasathwik6@gmail.com>
2026-05-15 01:19:27 +0100

    Replace variables with hardcoded values for instance

--- main.tf
+++ main.tf
@@ -2,19 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "name" {
-  default = "Level6-VM"
-}
-
 resource "aws_instance" "vm" {
   ami           = "ami-0c7217cdde317cfec"
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
 
   tags = {
-    Name = var.name
+    Name = "Level6-VM"
   }
 }
